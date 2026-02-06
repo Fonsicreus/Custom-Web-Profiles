c7f302d83449bf5e7d5d09021ce62e2ac7b1cb33
Fravelz <fravelz_@hotmail.com>
2026-02-06 05:54:54 -0500

    fix(styles and script): ....
    1. Added responsive design.
    
    2. Improve script (typing-chinese.js).

--- pages/king/scripts/typing-chinese.js
+++ pages/king/scripts/typing-chinese.js
@@ -3,31 +3,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let contain_text = document.querySelector('.advice-chinese')
 
-    for (let i = 0; i <= advice.length; i++) {
+    for (let i = 0; i < advice.length; i++) {
         let new_div = document.createElement('div')
 
-        if (i !== advice.length) new_div.id = `character-target-${i}`
-
-        else {
-            new_div.textContent = advice[advice.length - 1]
-        }
+        if (i < advice.length-1) new_div.id = `character-target-${i}`
+        else new_div.textContent = advice[advice.length - 1]
 
         contain_text.appendChild(new_div)
     }
 
-    let chars_hanzi_writer = new Array(advice.length)
+    let chars_hanzi_writer = []
 
     for (let i = 0; i < advice.length - 1; i++) {
-        chars_hanzi_writer[i] = HanziWriter.create(`character-target-${i}`, advice[i], {
-            width: 25,
-            height: 25,
-            padding: 0,
-            strokeColor: '#F00',
-            outlineColor: 'rgb(86, 5, 5)',
-            showCharacter: false,
-            strokeAnimationSpeed: 20,
-            delayBetweenStrokes: 100
-        });
+        chars_hanzi_writer.push(
+            HanziWriter.create(`character-target-${i}`, advice[i], {
+                width: 25,
+                height: 25,
+                padding: 0,
+                strokeColor: '#F00',
+                outlineColor: 'rgb(86, 5, 5)',
+                showCharacter: false,
+                strokeAnimationSpeed: 20,
+                delayBetweenStrokes: 100
+            })
+        )
     }
 
     // Function Recursive
